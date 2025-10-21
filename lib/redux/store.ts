924b2cd76a1a29a82c06000b7cc5ae957c7505a0
Samuele Patti <samuele.patti.job@gmail.com>
2025-10-21 10:13:53 +0200

    adada

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -1,21 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./features/api/apiSlice";
-import { authApiSlice } from "./features/auth/authApiSlice";
 import { externalApiSlice } from "./features/external/externalApiSlice";
 import authReducer from "./features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    [authApiSlice.reducerPath]: authApiSlice.reducer,
-    [externalApiSlice.reducerPath]: externalApiSlice.reducer, // NEW: External API reducer
+    [externalApiSlice.reducerPath]: externalApiSlice.reducer,
     auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(apiSlice.middleware)
-      .concat(authApiSlice.middleware)
-      .concat(externalApiSlice.middleware), // NEW: External API middleware
+      .concat(externalApiSlice.middleware),
   devTools: process.env.NODE_ENV !== "production",
 });
 
